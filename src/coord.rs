5e27baf64a1dcc456ef17e490eba543fda9518b4
p4ken <commit@p4ken.skr.jp>
2024-07-07 16:13:24 +0900

    Add constructors

--- src/coord.rs
+++ src/coord.rs
@@ -5,14 +5,17 @@ use std::ops::{Add, Div, Mul, Sub};
 #[derive(Debug, Clone, Copy, PartialEq, PartialOrd, Default)]
 pub struct LatLon<T = f64>(pub T, pub T);
 impl<T> LatLon<T> {
+    /// Returns latitude.
     pub fn lat(self) -> T {
         self.0
     }
 
+    /// Returns longitude.
     pub fn lon(self) -> T {
         self.1
     }
 
+    /// Returns self with function `f` applied to both lat and lon.
     pub fn map<U>(self, f: impl Fn(T) -> U) -> LatLon<U> {
         let [lat, lon] = self.as_array().map(f);
         LatLon(lat, lon)
@@ -23,7 +26,9 @@ impl<T> LatLon<T> {
     }
 }
 impl LatLon {
-    pub(crate) fn new<T: Into<f64>>(lat: T, lon: T) -> Self {
+    /// コンストラクタ。
+    /// Constructs with latitude and longitude.
+    pub fn new(lat: f64, lon: f64) -> Self {
         Self(lat.into(), lon.into())
     }
 
@@ -98,31 +103,40 @@ impl Div<f64> for LatLon {
 }
 
 /// 度分秒。
-/// Degrees, minutes, seconds.
+/// A tuple of degrees, minutes and seconds.
 #[derive(Debug, Clone, Copy, PartialEq, PartialOrd, Default)]
 pub struct Dms(pub i32, pub i32, pub f64);
 impl Dms {
-    // fn new<D: Into<i32>, M: Into<i32>, S: Into<f64>>(d: D, m: M, s: S) -> Self {
-    //     let d = d.into();
-    //     let m = m.into();
-    //     let s = s.into();
-    //     Self { d, m, s }
-    // }
+    /// Constructs with degrees, minutes and seconds.
+    pub fn new(d: i32, m: i32, s: f64) -> Self {
+        // It's not const fn because s may be Into<f64> in the future.
+        Self(d, m, s)
+    }
+
+    /// Returns degrees.
     pub fn d(self) -> i32 {
         self.0
     }
+
+    /// Returns minutes.
     pub fn m(self) -> i32 {
         self.1
     }
+
+    /// Returns seconds.
     pub fn s(self) -> f64 {
         self.2
     }
+
+    /// Constructs from decimal degrees.
     fn from_degrees(deg: f64) -> Self {
         let d = deg as i32;
         let m = (deg * 60. % 60.) as i32;
         let s = (deg * 3600.) % 60.;
         Self(d, m, s)
     }
+
+    /// Converts to decimal degrees.
     fn to_degrees(self) -> f64 {
         let Self(d, m, s) = self;
         f64::from(d) + f64::from(m) / 60. + s / 3_600.
